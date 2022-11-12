3c4c430ce213f97e1136d88a45318f4762d147cb
dalibein <swirxik01@mail.ru>
2022-11-12 11:15:20 +0800

    Semka

---
+++ resources/views/5.blade.php
@@ -0,0 +1,43 @@
+@extends('menu')
+
+
+@section('ocnov')
+<div class="wrapper">
+
+	<header>
+		<h1>6 колонки</h1>
+	</header>
+		
+<section class="columns">
+	
+	
+	
+	<div class="column">
+		<h2>1 колонка</h2>
+		<p>Это вторая страница.</p>
+	</div>
+    <div class="column">
+		<h2>2 колонка</h2>
+		<p>Это вторая страница.</p>
+	</div>
+	<div class="column">
+		<h2>3 колонка</h2>
+		<p>Это вторая страница.</p>
+	</div>
+	<div class="column">
+		<h2>4 колонка</h2>
+		<p>Это вторая страница.</p>
+	</div>
+	<div class="column">
+		<h2>5 колонка</h2>
+		<p>Это вторая страница.</p>
+	</div>
+    <div class="column">
+		<h2>6 колонка</h2>
+		<p>Это вторая страница.</p>
+	</div>
+  
+  
+	
+</section>	
+@endsection
