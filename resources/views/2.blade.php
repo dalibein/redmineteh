b8e2eb8e08b7476699e2f4a14f2c8912bf835e77
dalibein <swirxik01@mail.ru>
2022-11-12 09:44:26 +0800

    Ilya

---
+++ resources/views/2.blade.php
@@ -0,0 +1,30 @@
+@extends('menu')
+
+
+@section('ocnov')
+<div class="wrapper">
+
+	<header>
+		<h1>3 колонки</h1>
+	</header>
+		
+<section class="columns">
+	
+	
+	
+	<div class="column">
+		<h2>1 колонка</h2>
+		<p>Это третья страница.</p>
+	</div>
+    <div class="column">
+		<h2>2 колонка</h2>
+		<p>Это третья страница.</p>
+	</div>
+    <div class="column">
+		<h2>3 колонка</h2>
+		<p>Это третья страница.</p>
+	</div>
+  
+  
+	
+</section>	
